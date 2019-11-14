aa26cc92935f9bd719e27c6e0a25b24789072170
Matthias Mayer <matthias.ma.mayer@daimler.com>
2019-11-14 14:21:33 +0100

    Initial implementation

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/mfmayer/sync2put
+
+go 1.13
+
+require (
+	github.com/fsnotify/fsnotify v1.4.7
+	golang.org/x/sys v0.0.0-20191113165036-4c7a9d0fe056 // indirect
+)
---
+++ main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func putFile(filePath string, url string) {
+	if _, err := os.Stat(filePath); !os.IsNotExist(err) {
+		file, err := os.Open(filePath)
+		if err != nil {
+			log.Println("Error while opening: ", filePath)
+			return
+		}
+		baseName := filepath.Base(filePath)
+		req, err := http.NewRequest("PUT", url+baseName, file)
+		if err != nil {
+			log.Println("Error while creating PUT request: ", err)
+			return
+		}
+		res, err := http.DefaultClient.Do(req)
+		if err != nil {
+			log.Println("Error sending PUT request: ", err)
+			return
+		}
+		defer res.Body.Close()
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			log.Println("Error while reading response body: ", err)
+			return
+		}
+		log.Println(url+baseName, string(body))
+	}
+}
+
+func main() {
+	wd, _ := os.Getwd()
+	dir := flag.String("dir", wd, "Directory to sync")
+	url := flag.String("url", "http://192.168.200.1:3001/rsc/", "Target URL where to put to")
+	syncOnStart := flag.Bool("s", true, "Synchronize whole directory on start")
+	flag.Parse()
+
+	if _, err := os.Stat(*dir); os.IsNotExist(err) {
+		log.Fatal("Directory ", *dir, " doesn't exist")
+	}
+	if !strings.HasPrefix(*url, "http") {
+		log.Fatal("Url ", *url, " is invalid")
+	}
+	if !strings.HasSuffix(*url, "/") {
+		*url = *url + "/"
+	}
+
+	fmt.Println("dir: ", *dir)
+	fmt.Println("url: ", *url)
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+
+	if *syncOnStart {
+		files, err := ioutil.ReadDir(*dir)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, file := range files {
+			if !file.IsDir() {
+				putFile(*dir+"/"+file.Name(), *url)
+			}
+		}
+	}
+
+	duration := 100 * time.Millisecond
+	timer := time.NewTimer(duration)
+	go func() {
+		events := make(map[string]fsnotify.Event)
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				events[event.String()] = event
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+			case <-timer.C:
+				for _, event := range events {
+					if event.Op&fsnotify.Write == fsnotify.Write {
+						putFile(event.Name, *url)
+					}
+				}
+				events = make(map[string]fsnotify.Event)
+				timer.Reset(duration)
+			}
+		}
+	}()
+
+	err = watcher.Add(*dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
+}
