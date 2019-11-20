d442ef2acbd36995fe2246f81bf211f7c7ee413d
Matthias Mayer <matthias.ma.mayer@daimler.com>
2019-11-20 08:02:53 +0100

    Added appendFileName2URL flag

--- main.go
+++ main.go
@@ -16,14 +16,18 @@ import (
 
 var method *string
 
-func putFile(filePath string, url string) {
+func putFile(filePath string, url string, appendFileName bool) {
 	if _, err := os.Stat(filePath); !os.IsNotExist(err) {
 		file, err := os.Open(filePath)
 		if err != nil {
 			log.Println("Error while opening: ", filePath)
 			return
 		}
-		baseName := filepath.Base(filePath)
+		baseName := ""
+		if appendFileName {
+			baseName = filepath.Base(filePath)
+		}
+
 		req, err := http.NewRequest(*method, url+baseName, file)
 		if err != nil {
 			log.Println("Error while creating PUT request: ", err)
@@ -48,6 +52,7 @@ func main() {
 	wd, _ := os.Getwd()
 	dir := flag.String("dir", wd, "Directory to sync")
 	url := flag.String("url", "http://192.168.200.1:3001/rsc/", "Target URL where to put to")
+	appendFileName := flag.Bool("appendFileName2URL", true, "Append file name to URL")
 	method = flag.String("method", "PUT", "HTTP Method to use")
 	syncOnStart := flag.Bool("s", true, "Synchronize whole directory on start")
 	flag.Parse()
@@ -81,7 +86,7 @@ func main() {
 
 		for _, file := range files {
 			if !file.IsDir() {
-				putFile(*dir+"/"+file.Name(), *url)
+				putFile(*dir+"/"+file.Name(), *url, *appendFileName)
 			}
 		}
 	}
@@ -105,7 +110,7 @@ func main() {
 			case <-timer.C:
 				for _, event := range events {
 					if event.Op&fsnotify.Write == fsnotify.Write {
-						putFile(event.Name, *url)
+						putFile(event.Name, *url, *appendFileName)
 					}
 				}
 				events = make(map[string]fsnotify.Event)
