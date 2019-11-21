d1e7808d4e35ee92022880ba3e790fc82d2fce92
Matthias Mayer <matthias.ma.mayer@daimler.com>
2019-11-21 09:04:12 +0100

    Adding basic authentication

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 
 var method *string
 
-func putFile(filePath string, url string, appendFileName bool) {
+func putFile(filePath string, url string, appendFileName bool, user, pwd string) {
 	if _, err := os.Stat(filePath); !os.IsNotExist(err) {
 		file, err := os.Open(filePath)
 		if err != nil {
@@ -35,6 +35,9 @@ func putFile(filePath string, url string, appendFileName bool) {
 		if err != nil {
 			log.Println("Error while creating PUT request: ", err)
 			return
+		}
+		if len(user) > 0 || len(pwd) > 0 {
+			req.SetBasicAuth(user, pwd)
 		}
 		res, err := http.DefaultClient.Do(req)
 		if err != nil {
@@ -55,10 +58,21 @@ func main() {
 	wd, _ := os.Getwd()
 	dir := flag.String("dir", wd, "Directory to sync")
 	url := flag.String("url", "http://192.168.200.1:3001/rsc/", "Target URL where to put to")
+	auth := flag.String("auth", "", "Basic authentication in the form: \"<user>:<pwd\"")
 	appendFileName := flag.Bool("append", true, "Append file name to URL")
 	method = flag.String("method", "PUT", "HTTP Method to use")
 	syncOnStart := flag.Bool("s", true, "Synchronize whole directory on start")
 	flag.Parse()
+	user := ""
+	pwd := ""
+	if len(*auth) > 0 {
+		up := strings.Split(*auth, ":")
+		if len(up) != 2 {
+			log.Fatal("auth not given in form \"<user>:<pwd\"")
+		}
+		user = up[0]
+		pwd = up[1]
+	}
 
 	if _, err := os.Stat(*dir); os.IsNotExist(err) {
 		log.Fatal("Directory ", *dir, " doesn't exist")
@@ -86,7 +100,7 @@ func main() {
 
 		for _, file := range files {
 			if !file.IsDir() {
-				putFile(*dir+"/"+file.Name(), *url, *appendFileName)
+				putFile(*dir+"/"+file.Name(), *url, *appendFileName, user, pwd)
 			}
 		}
 	}
@@ -110,7 +124,7 @@ func main() {
 			case <-timer.C:
 				for _, event := range events {
 					if event.Op&fsnotify.Write == fsnotify.Write {
-						putFile(event.Name, *url, *appendFileName)
+						putFile(event.Name, *url, *appendFileName, user, pwd)
 					}
 				}
 				events = make(map[string]fsnotify.Event)
